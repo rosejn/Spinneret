b8d7a5c3c5b5bafea0474316f735ba2c3c76a0c3
Cyrus Hall <hallcp@boulder.(none)>
2006-12-14 15:43:08 +0100

    Working version

---
+++ rjserve/job_types.rb
@@ -0,0 +1,33 @@
+module RJServe
+module Jobs
+
+  class ShellExecute
+    def initialize
+      @command = nil
+    end
+
+    def parse_opt(opt, arg)
+      case opt
+      when '-jh'
+        puts """ -jh                     Prints this help.
+ -jc  <command_str>      An escaped version of the command string."""
+      when '-jc'
+        @command = arg
+      end
+    end
+
+    def run
+      `#{@command}`
+    end
+
+    def options
+      return GetoptlOng
+    end
+
+    def runnable?
+      return !@command.nil?
+    end
+  end
+
+end
+end
