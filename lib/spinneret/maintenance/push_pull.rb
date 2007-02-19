fbc61175d8479ba4fbb2bfb18f8a4084311a63cb
Cyrus Hall <hallcp@boulder.(none)>
2007-02-19 21:35:45 +0100

    Modified all maintenance protocols to make sure no more than the number of set nodes are tranfered per maintenance cycle.

--- lib/spinneret/maintenance/push_pull.rb
+++ lib/spinneret/maintenance/push_pull.rb
@@ -10,22 +10,22 @@ module Maintenance
       peers = @link_table.random_peers(NUM_NEIGHBOR_REQUESTS)
       send_packet(:neighbor_request, peers.map { | p | p.addr },
                   NeighborRequest.new(@addr, @nid,
-                                      @link_table.random_peers(@maintenance_size)))
+                                      @link_table.random_peers(@maintenance_size / 2)))
     end
 
     def handle_neighbor_request(pkt)
       send_packet(:neighbor_response, pkt.src,
                   NeighborResponse.new(@addr, @nid,
-                                       @link_table.random_peers(@maintenance_size)))
+                                       @link_table.random_peers(@maintenance_size / 2)))
       @link_table.store_peer(Peer.new(pkt.src, pkt.nid))
       handle_neighbor_response(pkt)
     end
 
     def handle_neighbor_response(pkt)
-#      printf("@%d\n", @sim.time) if @addr == 1641
-#      printf("before: %s\n", @link_table)  if @nid == 1641
+      printf("@%d\n", @sim.time) if @addr == 25
+      printf("before: %s\n", @link_table)  if @addr == 25
       pkt.neighbors.each {|n| @link_table.store_peer(n)}
-#      printf("after: %s\n", @link_table)  if @nid == 1641
+      printf("after: %s\n", @link_table)  if @addr == 25
     end
 
   end
