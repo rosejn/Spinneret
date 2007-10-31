720300a86fa8ac8fd69bccc8ff855eadd2bafa5d
Cyrus Hall <hallcp@boulder.(none)>
2007-10-31 14:04:14 +0100

    Fixed bug in graph helpers

--- lib/spinneret/rgl_helpers/helpers.rb
+++ lib/spinneret/rgl_helpers/helpers.rb
@@ -9,7 +9,7 @@ include RGL
 # This should really be type (digraph/unigraph) agnostic, but ehh, whatever 
 # for now.
 class DirectedAdjacencyGraph
-  EDGE_RE = /^\s*(\d+)->(\d+) ?([^;]*);/
+  EDGE_RE = /^\s*(\d+)\s*->\s*(\d+) ?([^;]*);/
   NODE_RE = /^\s*(\d+) ?([^;]*);/
 
   INFO_RE = /(\w+)="(\d+\.?\d*)"/
@@ -94,69 +94,13 @@ module Graph
     return nil
   end
 
-  # All of this is no longer used.  Depreicated, delete latter.
-=begin
-  def avg_path_length(n = 1000)
-    verts = vertices()
-
-    path_length = 0.0
-    n.times do
-      v2 = v1 = verts.rand()
-      v2 = verts.rand() while v2 == v1
-
-      iter = bfs_iterator(v1)
-      iter.attach_distance_map()
-      iter.set_to_end()   # do search, mark distances
-
-      length = iter.distance_to_root(v2)
-#      puts "#{v1} -> #{v2} => #{length}"
-      path_length += length
-    end
-
-    return path_length /= n.to_f
-  end
-
-  def cluster_coeff()
-    v = vertices()
-    v.inject(0) { | val, idx | val += node_cluster_coeff(idx) } / v.size.to_f
-  end
-
-  def node_cluster_coeff(v)
-    num_edges = 0
-    neighbors = Set.new
-    each_adjacent(v) do | k |
-      num_edges += 1
-      neighbors.add(k)
-    end
-
-    return 0 if num_edges <= 1
-
-#    puts "N: #{neighbors.to_a.sort.join(', ')}"
-
-    nofns = Set.new
-    neighbors.each do | k |
-#      puts "#{k}"
-      each_adjacent(k) do | kk |
-#        puts "  #{kk}"
-        # note that [kk, k] is considered a different edge
-        if(neighbors.member?(kk) && (directed? || !nofns.member?([kk, k])))
-           nofns.add([k, kk]) if neighbors.member?(kk)
-        end
-      end
-    end
-
-    return ((directed? ? 1.0 : 2.0) * nofns.size()) / 
-              (num_edges * (num_edges - 1)).to_f
-  end
-=end
-
   #[id=\"#{k.nid}\", visit_freq=\"#{k.visit_avg.avg}\"];\n"
   def vertex_props_dot(v)
     @vertex_properties ||= {}
     s = ""
     if @vertex_properties.has_key?(v)
       if @vertex_properties[v].size > 0
-        s << "[" 
+        s << " [" 
         s << @vertex_properties[v].map do | name, value |
           if value.class == Float
             value = sprintf("%20.20f", value)
@@ -177,7 +121,7 @@ module Graph
     s = ""
     if @edge_properties.has_key?(key)
       if @edge_properties[key].size > 0
-        s << "["
+        s << " ["
         s << @edge_properties[key].map do | name, value |
           if value.class == Float
             value = sprintf("%20.20f", value)
@@ -197,7 +141,7 @@ module Graph
     s = "digraph G {\n"
     graph_attrs.each {|name, val| s << "#{name}=\"#{val}\";\n"}
     v.each do | k |
-      s << "#{k.to_d} " << vertex_props_dot(k) << dot_node(k)
+      s << "#{k.to_d}" << vertex_props_dot(k) << dot_node(k)
     end
     s << "}\n"
   end
@@ -206,7 +150,7 @@ module Graph
     s = ""
     each_adjacent(k) do | kk | 
       if !kk.nil?
-        s << "#{k.to_d}->#{kk.to_d} " << edge_props_dot(k, kk)
+        s << "#{k.to_d}->#{kk.to_d}" << edge_props_dot(k, kk)
       end 
     end
 
